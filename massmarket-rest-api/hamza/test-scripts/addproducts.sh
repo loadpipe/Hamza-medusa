ca84b10bcdbdf868262fbbf04c34012d29dac598
Acer <john.kosinski@gmail.com>
2024-07-02 21:19:13 +0700

    added test scripts

--- massmarket-rest-api/hamza/test-scripts/addproducts.sh
+++ massmarket-rest-api/hamza/test-scripts/addproducts.sh
@@ -6,3 +6,37 @@
 #0x2bec4242fbb2803d4b8c937474b180fbe59fa1fb59d9052c5374867650f802dc: INDOOR/OUTDOOR WALL LAMP M - W100 X D4"
 #0x3df4d361c56898b08302fe6521c1b3bd2e2aae293475445c028c9b23a462d6fa: Tripod RGB Spine
 #0xa619cf448f807c7c83f2602eaa03cf616d80ab20a1723e55bcdfa7fe0d10d364: RGB LED Strips
+
+
+#curl --header "Content-Type: application/json" \
+#  --request POST \
+#  --data '{"storeId":"0x6df3380a85b85432e2884a05ae477f8847821de866f0e69594bab094127238a5","keycard":"0x66c34a40a9f6b0b947e888181a585af7ba6e751446fd239f66cd83ef79ac9b6a", "products": [{"name": "The Streamline Rift", "price": "145.00", "description": "Sleek Gaming Audio", "image": "https://images.hamza.biz/dalle_vr_2.webp"}]}' http://localhost:3001/api/product
+  
+#curl --header "Content-Type: application/json" \
+#  --request POST \
+#  --data '{"storeId":"0xe810f5bc3e3889bb1e6db9d4d8d2b486366ad99716732d74b734e6a4251e4ee9","keycard":"0xb405b8003a5e84ebf67edcbce30cf9e77e32ef17b41a7c9d0298b83f19cac9c2", "products": [{"name": "The Visionary", "price": "156.00", "description": "See the Unseen", "image": "https://images.hamza.biz/dalle_vr_headset3.webp"}]}' http://localhost:3001/api/product
+    
+#curl --header "Content-Type: application/json" \
+#  --request POST \
+#  --data '{"storeId":"0xb12e73bc2f6c6beff0a9538ee72dea5aa7c6626e7eb16ab08646528b55d77241","keycard":"0x28dadff504e9ba14d173cbd08aea650db2299783f58fab561616c52cbcc275ed", "products": [{"name": "LEGO Icons Corvette Classic Car", "price": "200.00", "description": "Corvette Classic Car Model Building Kit for Adults", "image": "https://images.hamza.biz/Lego/corvette/corvette_1.jpg"}]}' http://localhost:3001/api/product
+      
+#curl --header "Content-Type: application/json" \
+#  --request POST \
+#  --data '{"storeId":"0xa5529b05d40f7189e52988918bef584128f037b8289bc5deb4f02745546a74d4","keycard":"0x9e640178bc719feca2d8b4ed19a39c38c83c0fc398cf506e6558dbaa22365a17", "products": [{"name": "DJI Mini 3 Pro with Flagship Triple-Camera", "price": "221.00", "description": "Hasselblad Main Camera - Mavic 3 Pro has a 4/3 CMOS sensor, 24mm format equivalent, f/2.8-f/11, 20 MP. The Hasselblad 4K drone camera supports shooting RAW photos with a dynamic range of up to 12.8 stops.", "image": "https://images.hamza.biz/Drones/dji_mini_3_pro/dji_mini_3_tripod_1.jpg"}]}' http://localhost:3001/api/product
+      
+curl --header "Content-Type: application/json" \
+ --request POST \
+  --data '{"storeId":"0xb6a73bd3ed64d7aa8ab124e045be31a7f9e1add3a47c4371ccb707a75219a21d","keycard":"0x41f5dc26e0b62bd2c5ce3e4ed5cb3b153d7a7d87ae3f046f0be190dcad548097", "products": [{"name": "Dark Souls: The Board Game", "price": "58.00", "description": "Steamforged Games Dark Souls: The Board Game - A Board Game by Steamforged 1-4 Players - Board Games for Family 90-120 Minutes of Gameplay", "image": "https://images.hamza.biz/Board_Games/dark_souls/souls_1.jpg"}]}' http://localhost:3001/api/product
+      
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"storeId":"0x3acac564f40baf2d070f2fd9f20ec42c1ed5b84c62b91c0a6dca0b7ee5cb2027","keycard":"0xee6372c11170059d799ae237a283c91c135e15fd469e3899ce53d66d8b72cf60", "products": [{"name": "ihuan Ventilated Weight Lifting Gym Workout Gloves", "price": "59.00", "description": "ihuan Ventilated Weight Lifting Gym Workout Gloves with Wrist Wrap Support", "image": "https://images.hamza.biz/Workout/glove/glove_1.jpg"}]}' http://localhost:3001/api/product
+  
+#curl --header "Content-Type: application/json" \
+# --request POST \
+#  --data '{"storeId":"","keycard":"", "products": [{"name": "", "price": ".00", "description": "", "image": ""}]}' http://localhost:3001/api/product
+      
+#curl --header "Content-Type: application/json" \
+#  --request POST \
+#  --data '{"storeId":"","keycard":"", "products": [{"name": "", "price": ".00", "description": "", "image": ""}]}' http://localhost:3001/api/product
+  
